6e64be50d47c5f3c46f407d61b67913f19990c53
CCSFire <carlo.chionetti@studbocconi.it>
2022-04-25 13:28:23 +0200

    commit

--- custom_path_autocompleter.py
+++ custom_path_autocompleter.py
@@ -22,3 +22,44 @@ readline.set_completer(completer)
 
 
 input('>>> ')
+
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+from scipy.cluster.vq import whiten
+from scipy.cluster.vq import kmeans
+import pandas as pd
+ 
+satellite_image = img.imread('satellite_image.jpg')
+ 
+r = []
+g = []
+b = []
+for row in satellite_image:
+    for temp_r, temp_g, temp_b, temp in row:
+        r.append(temp_r)
+        g.append(temp_g)
+        b.append(temp_b)
+  
+satellite_image_df = pd.DataFrame({'red' : r, 'green' : g, 'blue' : b})
+ 
+satellite_image_df['scaled_color_red'] = whiten(satellite_image_df['red'])
+satellite_image_df['scaled_color_blue'] = whiten(satellite_image_df['blue'])
+satellite_image_df['scaled_color_green'] = whiten(satellite_image_df['green'])
+ 
+cluster_centers, _ = kmeans(satellite_image_df[['scaled_color_red', 'scaled_color_blue', 'scaled_color_green']], 3)
+ 
+dominant_colors = []
+ 
+red_std, green_std, blue_std = satellite_image_df[['red', 'green', 'blue']].std()
+ 
+for cluster_center in cluster_centers:
+    red_scaled, green_scaled, blue_scaled = cluster_center
+    dominant_colors.append((
+        red_scaled * red_std / 255,
+        green_scaled * green_std / 255,
+        blue_scaled * blue_std / 255
+    ))
+ 
+plt.imshow([dominant_colors])
+plt.show()
+
