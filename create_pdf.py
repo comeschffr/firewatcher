0931162ffa3b1eed7a12a8a0f3bce9bf3ccb9230
hmz-97 <40907792+hmz-97@users.noreply.github.com>
2022-05-04 01:35:24 +0200

    merging graphs and pdf generator

--- create_pdf.py
+++ create_pdf.py
@@ -11,6 +11,66 @@ mock_graph_links = {
     "wind": "images/wind.jpeg"
 }
 
+def create_temperature_chart(weather_data: dict) -> str:
+    graph_filename = "resources/temperature_chart.png"
+    temperature_data = weather_data['temperature']
+    day_labels = [dt.strftime("%m/%d") for dt in weather_data['dates']]
+
+    plt.bar(day_labels, temperature_data, color="firebrick")  
+    plt.title(str(len(temperature_data)) + '-day Temperature Forecast')
+    plt.ylabel("°C")
+    plt.savefig(graph_filename)
+    plt.clf()
+
+    return graph_filename
+
+def create_humidity_rain_chart(weather_data: dict) -> str:
+    graph_filename = "resources/hum_rain_chart.png"
+    hum_data = weather_data['humidity']
+    rain_data = weather_data['rain']
+    day_labels = [dt.strftime("%m/%d") for dt in weather_data['dates']]
+
+    fig, ax = plt.subplots()
+    ax.bar(day_labels, rain_data, color="aqua", label="rain")
+    ax.set_ylabel("Rain volume (in mm)")
+    ax.legend(loc=0)
+    ax2 = ax.twinx()
+    ax2.plot(day_labels, hum_data, marker='o', color="lightsteelblue", label="humidity")
+    ax2.set_ylabel("Humidity (in %)")
+    ax2.legend(loc=0)
+    plt.title("Rain and Humidity Forecast")
+    fig.savefig(graph_filename)
+    plt.clf()
+
+    return graph_filename
+
+def create_wind_chart(weather_data: dict) -> str:
+    graph_filename = "resources/wind_chart.png"
+    wind_data = weather_data['wind']
+    day_labels = [dt.strftime("%m/%d") for dt in weather_data['dates']]
+
+    plt.plot(day_labels, wind_data, marker='o', color="olivedrab")
+    plt.title("Wind forecast")
+    plt.ylabel("m/s")
+    plt.savefig(graph_filename)
+    plt.clf()
+
+    return graph_filename
+
+def create_sunlight_chart(weather_data: dict) -> str:
+    graph_filename = "resources/sunlight_chart.png"
+    sunlight_data = weather_data['sunlight']
+    day_labels = [dt.strftime("%m/%d") for dt in weather_data['dates']]
+
+    plt.bar(day_labels, sunlight_data, color="gold")
+    plt.title("Sunlight forecast")
+    plt.ylabel("UV index")
+    plt.ylim(top=10)
+    plt.savefig(graph_filename)
+    plt.clf()
+
+    return graph_filename
+
 def resize_and_convert(path, newsize = (250,250)):
     img_PIL = Image.open(path)
     img_PIL = img_PIL.resize(newsize)
@@ -22,7 +82,7 @@ def create_pdf_report(graphs_link: dict):
     '''
     Creates a PDF file with the information on the place and graphics
     '''
-    
+
     # create pdf file
 
     margin_x = 1 * cm
@@ -58,5 +118,3 @@ def create_pdf_report(graphs_link: dict):
     canvas.save()
 
     return None
-
-create_pdf_report(mock_graph_links)
